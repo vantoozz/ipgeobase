43aee28fba2d5291c1093e98a40cf49c6331d8f0
Ivan Nikitin <vantoozz@gmail.com>
2014-01-03 15:42:36 +0400

    ~ optimize seeder

--- src/Vantoozz/Ipgeobase/IpgeobaseSeeder.php
+++ src/Vantoozz/Ipgeobase/IpgeobaseSeeder.php
@@ -11,10 +11,13 @@ class IpgeobaseSeeder extends Seeder
     public function run()
     {
         if (file_exists(__DIR__ . '/cities.txt') && file_exists(__DIR__ . '/cidr_optim.txt')) {
+
             DB::table('ipgeobase_cities')->delete();
 
             $file = file(__DIR__ . '/cities.txt');
             $pattern = '#(\d+)\s+(.*?)\t+(.*?)\t+(.*?)\t+(.*?)\s+(.*)#';
+
+            DB::beginTransaction();
             foreach ($file as $row) {
                 if (preg_match($pattern, $row, $out)) {
                     DB::table('ipgeobase_cities')->insert(
@@ -25,17 +28,19 @@ class IpgeobaseSeeder extends Seeder
                             'district' => $out[4],
                             'lat' => $out[5],
                             'lng' => $out[6],
-                            'country'=>''
+                            'country' => ''
                         )
                     );
                 }
             }
-
+            DB::commit();
 
             DB::table('ipgeobase_base')->delete();
 
             $file = file(__DIR__ . '/cidr_optim.txt');
             $pattern = '#(\d+)\s+(\d+)\s+(\d+\.\d+\.\d+\.\d+)\s+-\s+(\d+\.\d+\.\d+\.\d+)\s+(\w+)\s+(\d+|-)#';
+
+            DB::beginTransaction();
             foreach ($file as $row) {
                 if (preg_match($pattern, $row, $out)) {
                     DB::table('ipgeobase_base')->insert(
@@ -50,17 +55,19 @@ class IpgeobaseSeeder extends Seeder
                     );
                 }
             }
+            DB::commit();
 
             $cities = DB::table('ipgeobase_cities')
                 ->join('ipgeobase_base', 'ipgeobase_cities.id', '=', 'ipgeobase_base.city_id')
                 ->select('ipgeobase_cities.id', 'ipgeobase_base.country')->get();
-            
+
+            DB::beginTransaction();
             foreach ($cities as $city) {
                 DB::table('ipgeobase_cities')
                     ->where('id', $city->id)
                     ->update(array('country' => $city->country));
             }
-
+            DB::commit();
         }
     }
 
